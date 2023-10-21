ff0df2d2098ca81273664e36b1eeab43b31efeb0
Haruncan Atalı <77530565+haruncanatali@users.noreply.github.com>
2023-10-21 12:03:48 +0300

    Add files via upload

---
+++ odev-8.sql
@@ -0,0 +1,74 @@
+-- 1
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    birthday DATE,
+    email VARCHAR(100)
+);
+
+-- 2
+insert into employee (id, name, birthday, email) values (1, 'Farlee', '11/14/2022', 'fdoylend0@myspace.com');
+insert into employee (id, name, birthday, email) values (2, 'Gardiner', '7/15/2023', 'gpatters1@who.int');
+insert into employee (id, name, birthday, email) values (3, 'Willetta', '2/23/2023', 'wyankov2@surveymonkey.com');
+insert into employee (id, name, birthday, email) values (4, 'Aundrea', '7/23/2023', 'agallafant3@theatlantic.com');
+insert into employee (id, name, birthday, email) values (5, 'Elie', '3/13/2023', 'esandell4@indiatimes.com');
+insert into employee (id, name, birthday, email) values (6, 'Valentine', '3/11/2023', 'vmulvin5@fema.gov');
+insert into employee (id, name, birthday, email) values (7, 'Tessy', '8/22/2023', 'tcrepin6@yale.edu');
+insert into employee (id, name, birthday, email) values (8, 'Beulah', '9/23/2023', 'bligerton7@twitpic.com');
+insert into employee (id, name, birthday, email) values (9, 'Nara', '12/2/2022', 'nsignorelli8@cnbc.com');
+insert into employee (id, name, birthday, email) values (10, 'Hurley', '11/16/2022', 'hhaszard9@purevolume.com');
+insert into employee (id, name, birthday, email) values (11, 'Gunar', '6/16/2023', 'gstodharta@bigcartel.com');
+insert into employee (id, name, birthday, email) values (12, 'Mil', '6/19/2023', 'moflanneryb@who.int');
+insert into employee (id, name, birthday, email) values (13, 'Rosaleen', '9/22/2023', 'rszymanowiczc@bravesites.com');
+insert into employee (id, name, birthday, email) values (14, 'Valerie', '3/31/2023', 'vtinsond@ibm.com');
+insert into employee (id, name, birthday, email) values (15, 'Davin', '1/3/2023', 'ddanbyee@sbwire.com');
+insert into employee (id, name, birthday, email) values (16, 'Jeanie', '9/29/2023', 'jcausticf@cbc.ca');
+insert into employee (id, name, birthday, email) values (17, 'Allis', '5/12/2023', 'athirkettleg@jiathis.com');
+insert into employee (id, name, birthday, email) values (18, 'Evelin', '11/3/2022', 'esilverthornh@netvibes.com');
+insert into employee (id, name, birthday, email) values (19, 'Windy', '12/2/2022', 'wroebyi@bloomberg.com');
+insert into employee (id, name, birthday, email) values (20, 'Hillie', '4/2/2023', 'hmaragesj@google.com.hk');
+insert into employee (id, name, birthday, email) values (21, 'Carol-jean', '7/14/2023', 'cmerredyk@odnoklassniki.ru');
+insert into employee (id, name, birthday, email) values (22, 'Kettie', '8/28/2023', 'kwainl@elegantthemes.com');
+insert into employee (id, name, birthday, email) values (23, 'Andre', '7/11/2023', 'avenusm@ebay.com');
+insert into employee (id, name, birthday, email) values (24, 'Leontine', '7/29/2023', 'llonglandn@uiuc.edu');
+insert into employee (id, name, birthday, email) values (25, 'Retha', '10/18/2023', 'rbentoteo@simplemachines.org');
+insert into employee (id, name, birthday, email) values (26, 'Gale', '9/6/2023', 'ggrandhamp@weebly.com');
+insert into employee (id, name, birthday, email) values (27, 'Sharleen', '2/18/2023', 'sscrammageq@storify.com');
+insert into employee (id, name, birthday, email) values (28, 'Mellie', '7/31/2023', 'mhamblingtonr@exblog.jp');
+insert into employee (id, name, birthday, email) values (29, 'Cirilo', '1/5/2023', 'cpickovers@washingtonpost.com');
+insert into employee (id, name, birthday, email) values (30, 'Loraine', '6/5/2023', 'lbatont@wiley.com');
+insert into employee (id, name, birthday, email) values (31, 'Colman', '9/7/2023', 'choffu@bbc.co.uk');
+insert into employee (id, name, birthday, email) values (32, 'Dionisio', '9/3/2023', 'dbantingv@about.me');
+insert into employee (id, name, birthday, email) values (33, 'Klemens', '6/26/2023', 'ktrevesw@cornell.edu');
+insert into employee (id, name, birthday, email) values (34, 'Fayette', '2/4/2023', 'fwhitsunx@ocn.ne.jp');
+insert into employee (id, name, birthday, email) values (35, 'Godfry', '5/27/2023', 'gmckenneyy@etsy.com');
+insert into employee (id, name, birthday, email) values (36, 'Letisha', '1/24/2023', 'lcraythornez@sciencedirect.com');
+insert into employee (id, name, birthday, email) values (37, 'Nolana', '2/18/2023', 'nharmour10@exblog.jp');
+insert into employee (id, name, birthday, email) values (38, 'Conn', '1/27/2023', 'crowth11@unesco.org');
+insert into employee (id, name, birthday, email) values (39, 'Jon', '2/1/2023', 'jkunath12@soundcloud.com');
+insert into employee (id, name, birthday, email) values (40, 'Wynn', '12/5/2022', 'wswanborough13@sphinn.com');
+insert into employee (id, name, birthday, email) values (41, 'Jane', '2/10/2023', 'jwyness14@npr.org');
+insert into employee (id, name, birthday, email) values (42, 'Joycelin', '12/18/2022', 'jrivers15@so-net.ne.jp');
+insert into employee (id, name, birthday, email) values (43, 'Chandal', '4/18/2023', 'cryder16@howstuffworks.com');
+insert into employee (id, name, birthday, email) values (44, 'Lucienne', '7/8/2023', 'liori17@ucla.edu');
+insert into employee (id, name, birthday, email) values (45, 'Bonnie', '3/4/2023', 'bbritten18@tinyurl.com');
+insert into employee (id, name, birthday, email) values (46, 'Sky', '11/9/2022', 'svedeneev19@ucla.edu');
+insert into employee (id, name, birthday, email) values (47, 'Rudolf', '6/21/2023', 'rpreshaw1a@ocn.ne.jp');
+insert into employee (id, name, birthday, email) values (48, 'Frieda', '2/2/2023', 'fhardisty1b@topsy.com');
+insert into employee (id, name, birthday, email) values (49, 'Ninon', '2/28/2023', 'nklaves1c@mapy.cz');
+insert into employee (id, name, birthday, email) values (50, 'Kerrill', '7/31/2023', 'kgroger1d@tiny.cc');
+
+-- 3
+update "employee" set "name" = "name" + "x" where "id" < 2
+update "employee" set "birthday" = "birthday"::date + interval '2 days' where "id" < 5
+update "employee" set "birthday" = "birthday"::date + interval '1 days' where "id" < 5 and "id" > 3
+update "employee" set "birthday" = "birthday"::date + interval '2 days' where "id" < 15 and "id" > 10
+update "employee" set "birthday" = "birthday"::date + interval '3 days' where "id" < 20 and "id" > 15
+
+-- 4
+delete from "employee" where length("name") > 20
+delete from "employee" where "id" > 58
+delete from "employee" where length("email") > 100
+delete from "employee" where "id" < 5
+delete from "employee" where "id" = 44
+ 
