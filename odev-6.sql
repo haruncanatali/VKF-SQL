a8b06d6befcd200f30d777403a079fc5316a763f
Haruncan Atalı <77530565+haruncanatali@users.noreply.github.com>
2023-10-21 11:38:08 +0300

    Add files via upload

---
+++ odev-6.sql
@@ -0,0 +1,11 @@
+-- 1
+select avg("rental_rate") from "film"
+
+-- 2
+select count(*) from "film" where "title" like 'C%'
+
+-- 3
+select "length" from "film" where "rental_rate" = 0.99 order by "length" desc limit 1 
+
+-- 4
+select count(distinct "replacement_cost") from "film" where "length" > 150
